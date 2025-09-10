efdf805c76650e848cda322395907882301fe47c
simonliuyuwei_apmic <simon3458@gmail.com>
2025-09-10 10:36:14 +0800

    feat/skip_special_tokens

--- adk_agent/model/litellm_model/model_config.py
+++ adk_agent/model/litellm_model/model_config.py
@@ -10,9 +10,18 @@ API_BASE                     = os.getenv("LITELLM_MODEL_API_BASE")
 API_KEY                      = os.getenv("LITELLM_MODEL_API_KEY")
 MODEL_NAME                   = os.getenv("LITELLM_MODEL_MODEL_NAME")
 
-# 初始化 LiteLlm 模型
-litellm_model = LiteLlm(
-    model=MODEL_NAME,
-    api_base=API_BASE,
-    api_key=API_KEY
-)
+if bool(os.getenv("LITELLM_MODEL_SPECIAL_TOKENS")):
+    # 初始化 LiteLlm 模型
+    litellm_model = LiteLlm(
+        model=MODEL_NAME,
+        api_base=API_BASE,
+        api_key=API_KEY,
+    )
+else:
+    # 初始化 LiteLlm 模型，並設定 skip_special_tokens 為 False
+    litellm_model = LiteLlm(
+        model=MODEL_NAME,
+        api_base=API_BASE,
+        api_key=API_KEY,
+        extra_body={"skip_special_tokens": False}
+    )
